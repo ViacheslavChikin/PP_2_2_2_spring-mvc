786f093ffcef18b07601ea8275361908f41109d8
ViacheslavChikin <slv3211@gmail.com>
2024-04-16 18:30:27 +0300

    add dependency injection

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,18 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImp;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static web.model.Car.cars;
 
 @Controller
 public class CarController {
-    CarService carService = new CarServiceImp();
+    CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,5 +1,6 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.List;
@@ -7,8 +8,11 @@ import java.util.stream.Collectors;
 
 import static web.model.Car.cars;
 
+
+@Component
 public class CarServiceImp implements CarService {
 
-        public  List<Car> carsReturn(int x) {
-            return cars.stream().limit(x).collect(Collectors.toList()); }
+    public List<Car> carsReturn(int x) {
+        return cars.stream().limit(x).collect(Collectors.toList());
+    }
 }
